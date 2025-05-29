85b6ca5f1a461259a849589b9602e095811fc914
Alex Freitas <126065165+AlexFreitas24@users.noreply.github.com>
2025-05-29 16:09:48 +0100

    UpdateSendReceiver

--- Reciever/Reciever/Program.cs
+++ Reciever/Reciever/Program.cs
@@ -1,8 +1,7 @@
 ﻿using RabbitMQ.Stream.Client.Reliable;
 using RabbitMQ.Stream.Client;
-using System.IO;
 using System.Text;
-
+using System.Text.Json;
 
 var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
 
@@ -16,11 +15,28 @@ var consumer = await Consumer.Create(new ConsumerConfig(streamSystem, "hello_str
     OffsetSpec = new OffsetTypeFirst(),
     MessageHandler = async (stream, _, _, message) =>
     {
-        Console.WriteLine($"Stream: {stream} - " +
-                          $"Received message: {Encoding.UTF8.GetString(message.Data.Contents)}");
+        var json = Encoding.UTF8.GetString(message.Data.Contents);
+        var peca = JsonSerializer.Deserialize<Peca>(json);
+
+        if (peca != null && peca.Codigo_Resultado != "01")
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[FALHA] Código: {peca.Codigo_Peca} | Data: {peca.Data_Producao} | Resultado: {peca.Codigo_Resultado}");
+            Console.ResetColor();
+        }
+
         await Task.CompletedTask;
     }
 });
 
-Console.WriteLine("Press [enter] to exit.");
+Console.WriteLine("A receber dados de produção via Stream. Enter para sair.");
 Console.ReadLine();
+
+record Peca
+{
+    public string Codigo_Peca { get; set; }
+    public string Data_Producao { get; set; }
+    public string Hora_Producao { get; set; }
+    public int Tempo_Producao { get; set; }
+    public string Codigo_Resultado { get; set; }
+}
--- Sender/Sender/Program.cs
+++ Sender/Sender/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text;
 using RabbitMQ.Stream.Client;
 using RabbitMQ.Stream.Client.Reliable;
+using System.Text.Json;
 
 var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
 
@@ -11,10 +12,26 @@ await streamSystem.CreateStream(new StreamSpec("hello_stream")
 
 var producer = await Producer.Create(new ProducerConfig(streamSystem, "hello_stream"));
 
+var random = new Random();
+var codigosResultado = new[] { "01", "02", "03", "04", "05", "06" };
+var prefixos = new[] { "aa", "ab", "ba", "bb" };
+
 for (var i = 0; i < 1000; i++)
 {
-    // Send a message to the stream
-    await producer.Send(new Message(Encoding.UTF8.GetBytes($"Hello, World {i}")));
-    Console.WriteLine($"Sent message {i}");
+    var peca = new
+    {
+        Codigo_Peca = $"{prefixos[random.Next(0, 4)]}{random.Next(100000, 999999)}",
+        Data_Producao = DateTime.Today.ToString("yyyy-MM-dd"),
+        Hora_Producao = DateTime.Now.ToString("HH:mm:ss"),
+        Tempo_Producao = random.Next(10, 51),
+        Codigo_Resultado = codigosResultado[random.Next(0, 6)]
+    };
+
+    var json = JsonSerializer.Serialize(peca);
+    await producer.Send(new Message(Encoding.UTF8.GetBytes(json)));
+    Console.WriteLine($"[SENT] {json}");
     Thread.Sleep(1000);
 }
+
+
+
