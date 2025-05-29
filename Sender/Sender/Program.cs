6b9438bcb4318df9d3e4ba69bf48f1cfde1d9f49
Alex Freitas <126065165+AlexFreitas24@users.noreply.github.com>
2025-05-29 15:43:14 +0100

    Sender

---
+++ Sender/Sender/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using RabbitMQ.Stream.Client;
+using RabbitMQ.Stream.Client.Reliable;
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+
+await streamSystem.CreateStream(new StreamSpec("hello_stream")
+{
+    MaxLengthBytes = 5_000_000_000
+});
+
+var producer = await Producer.Create(new ProducerConfig(streamSystem, "hello_stream"));
+
+for (var i = 0; i < 1000; i++)
+{
+    // Send a message to the stream
+    await producer.Send(new Message(Encoding.UTF8.GetBytes($"Hello, World {i}")));
+    Console.WriteLine($"Sent message {i}");
+    Thread.Sleep(1000);
+}
