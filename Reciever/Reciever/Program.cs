70842cc50019a764177d1613afc3e4bb7acb5dd9
Alex Freitas <126065165+AlexFreitas24@users.noreply.github.com>
2025-05-29 15:44:27 +0100

    Reciever

---
+++ Reciever/Reciever/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Stream.Client.Reliable;
+using RabbitMQ.Stream.Client;
+using System.IO;
+using System.Text;
+
+
+var streamSystem = await StreamSystem.Create(new StreamSystemConfig());
+
+await streamSystem.CreateStream(new StreamSpec("hello_stream")
+{
+    MaxLengthBytes = 5_000_000_000
+});
+
+var consumer = await Consumer.Create(new ConsumerConfig(streamSystem, "hello_stream")
+{
+    OffsetSpec = new OffsetTypeFirst(),
+    MessageHandler = async (stream, _, _, message) =>
+    {
+        Console.WriteLine($"Stream: {stream} - " +
+                          $"Received message: {Encoding.UTF8.GetString(message.Data.Contents)}");
+        await Task.CompletedTask;
+    }
+});
+
+Console.WriteLine("Press [enter] to exit.");
+Console.ReadLine();
